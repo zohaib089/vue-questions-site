c3f583724cfa63dea92bf1faaf2d69cc0563c400
zohaib younis <zabi.jarral08@gmail.com>
2019-12-24 16:44:42 +0100

    First ever basic vue project been finished

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,74 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header :numCorrect="numCorrect" :numTotal="numTotal" />
+    <b-container class="mt-5 bv-example-row">
+      <b-row>
+        <b-col sm="10" offset="1">
+          <QuestionBox
+            v-if="questions.length"
+            :currentQuestion="questions[index]"
+            :next="next"
+            :increment="increment"
+          />
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/Header.vue";
+import QuestionBox from "./components/QuestionBox.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Header,
+    QuestionBox
+  },
+  data() {
+    return {
+      questions: [],
+      index: 0,
+      numCorrect: 0,
+      numTotal: 0
+    };
+  },
+  methods: {
+    next() {
+      this.index++;
+    },
+    increment(isCorrect) {
+      if (isCorrect) {
+        this.numCorrect++;
+      }
+      this.numTotal++;
+    }
+  },
+  mounted: function() {
+    fetch(
+      "https://opentdb.com/api.php?amount=10&category=18&difficulty=easy&type=multiple",
+      {
+        method: "get"
+      }
+    )
+      .then(response => {
+        return response.json();
+      })
+      .then(resData => {
+        this.questions = resData.results;
+      });
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <b-nav tabs>
+      <b-nav-item disabled>
+        <b>Vue Quiz App</b>
+      </b-nav-item>
+      <b-nav-item>Counter: {{numCorrect}}/{{numTotal}}</b-nav-item>
+    </b-nav>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["numCorrect", "numTotal"]
+};
+</script>
